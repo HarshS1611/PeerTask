0374a9fb27007668fcec74c36fa190a67ce945f6
pranavvdesai <pranavdesai.psd@gmail.com>
2023-02-12 18:38:57 +0530

    getter functions

--- src/blockchain/contracts/JobPortal.sol
+++ src/blockchain/contracts/JobPortal.sol
@@ -1,187 +1,143 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.17;
 
-contract JobPortal {
+contract JobPortal{
     struct Project {
-        uint256 projectId;
+        uint projectId;
         address projectManager;
-        mapping(uint256 => Task) tasks; //Task[] tasks;
-        uint256 taskCount;
+        mapping(uint => Task) tasks; //Task[] tasks;
+        uint taskCount;
     }
 
-    struct Task {
-        uint256 taskId;
-        uint256 stakedAmount;
-        mapping(uint256 => Proposal) proposals; //Proposal[] proposals;
+     struct Task {
+        uint taskId;
+        uint stakedAmount;
+        mapping(uint => Proposal) proposals; //Proposal[] proposals;
         address selectedWorker;
-        uint256 proposalCount;
+        uint proposalCount;
         bool completed;
+        bool reviewed;
     }
 
     struct Proposal {
         address freelancer;
-        uint256 bidAmount;
+        uint bidAmount;
         string motivation;
     }
 
-    mapping(uint256 => Project) projects; //Project[] public projects;
-    uint256 projectCount;
+    mapping(uint => Project) public projects; //Project[] public projects;
+    uint public projectCount;
+    
+    event NewProject(uint projectId, address projectManager);
+    event NewTask(uint projectId, uint taskId, uint stakedAmount);
+    event NewProposal(uint projectId, uint taskId, address freelancer, uint bidAmount);
+    event WorkerSelected(uint projectId, uint taskId, address worker);
+    event TaskCompleted(uint projectId, uint taskId);
+    event ReviewCompleted(uint projectId, uint taskId);
+    event PaymentReleased(uint projectId, uint taskId, address worker, uint amount);
 
-    event NewProject(uint256 projectId, address projectManager);
-    event NewTask(uint256 projectId, uint256 taskId, uint256 stakedAmount);
-    event NewProposal(
-        uint256 projectId,
-        uint256 taskId,
-        address freelancer,
-        uint256 bidAmount
-    );
-    event WorkerSelected(uint256 projectId, uint256 taskId, address worker);
-    event TaskCompleted(uint256 projectId, uint256 taskId);
-    event PaymentReleased(
-        uint256 projectId,
-        uint256 taskId,
-        address worker,
-        uint256 amount
-    );
-
-    function createProject(address projectManager) public {
+     function createProject(address projectManager) public {
         projects[projectCount].projectId = projectCount;
         projects[projectCount].projectManager = projectManager;
         projects[projectCount].taskCount = 0;
 
         projectCount++;
 
-        emit NewProject(projectCount - 1, projectManager);
+        emit NewProject(projectCount-1, projectManager);
     }
 
-    function getCurrentProjectId() public view returns (uint256) {
+    function getCurrentProjectId() public view returns (uint) {
         return projectCount - 1;
     }
 
-    function createTask(uint256 projectId, uint256 stakedAmount) public {
-        require(
-            msg.sender == projects[projectId].projectManager,
-            "Only project manager can create tasks"
-        );
-
-        projects[projectId]
-            .tasks[projects[projectId].taskCount]
-            .taskId = projects[projectId].taskCount;
-        projects[projectId]
-            .tasks[projects[projectId].taskCount]
-            .stakedAmount = stakedAmount;
-        projects[projectId]
-            .tasks[projects[projectId].taskCount]
-            .completed = false;
-        projects[projectId].taskCount++;
-
-        emit NewTask(
-            projectId,
-            projects[projectId].taskCount - 1,
-            stakedAmount
-        );
+    // get the tasks of a project
+    function getTaskCountByProjectId(uint projectId) public view returns (uint) {
+        return projects[projectId].taskCount;
     }
 
-    // function getTasksByProjectId(uint projectId) public view returns (Task[] memory) {
-    //     return projects[projectId].tasks;
-    // }
+    function getTaskData(uint projectId, uint taskId) public view returns (
+        uint Id,
+        uint stakedAmount,
+        uint proposalCount,
+        address worker,
+        bool isComplete,
+        bool isReviewed
+    ) {
+        Task storage task = projects[projectId].tasks[taskId];
+        return (task.taskId, task.stakedAmount, task.proposalCount, task.selectedWorker, task.completed, task.reviewed);
+    }
 
-    function sendProposal(
-        uint256 projectId,
-        uint256 taskId,
-        uint256 bidAmount,
-        string memory motivation
-    ) public {
-        uint256 proposalCount = projects[projectId].tasks[taskId].proposalCount;
-        address selectedWorker = projects[projectId]
-            .tasks[taskId]
-            .selectedWorker;
 
-        require(
-            selectedWorker == address(0),
-            "Freelancer has already been selected for this task"
-        );
+    function createTask(uint projectId, uint stakedAmount) public {
+        require(msg.sender == projects[projectId].projectManager, "Only project manager can create tasks");
 
-        projects[projectId]
-            .tasks[taskId]
-            .proposals[proposalCount]
-            .freelancer = msg.sender;
-        projects[projectId]
-            .tasks[taskId]
-            .proposals[proposalCount]
-            .bidAmount = bidAmount;
-        projects[projectId]
-            .tasks[taskId]
-            .proposals[proposalCount]
-            .motivation = motivation;
+        projects[projectId].tasks[projects[projectId].taskCount].taskId = projects[projectId].taskCount;
+        projects[projectId].tasks[projects[projectId].taskCount].stakedAmount = stakedAmount;
+        projects[projectId].tasks[projects[projectId].taskCount].completed = false;
+        projects[projectId].tasks[projects[projectId].taskCount].reviewed = false;
+        projects[projectId].taskCount++;
+
+        emit NewTask(projectId, projects[projectId].taskCount-1, stakedAmount);
+    }
+
+    function sendProposal(uint projectId, uint taskId, uint bidAmount, string memory motivation) public {
+        uint proposalCount = projects[projectId].tasks[taskId].proposalCount;
+        address selectedWorker = projects[projectId].tasks[taskId].selectedWorker;
+
+        require(selectedWorker == address(0), "Freelancer has already been selected for this task");
+        
+        projects[projectId].tasks[taskId].proposals[proposalCount].freelancer = msg.sender;
+        projects[projectId].tasks[taskId].proposals[proposalCount].bidAmount = bidAmount;
+        projects[projectId].tasks[taskId].proposals[proposalCount].motivation = motivation;
         projects[projectId].tasks[taskId].proposalCount++;
 
         emit NewProposal(projectId, taskId, msg.sender, bidAmount);
     }
 
-    function getProposalsByTaskId(uint256 projectId, uint256 taskId)
-        public
-        view
-        returns (Proposal[] memory)
-    {
-        Proposal[] memory proposals = new Proposal[](
-            projects[projectId].tasks[taskId].proposalCount
-        );
-        for (
-            uint256 i = 0;
-            i < projects[projectId].tasks[taskId].proposalCount;
-            i++
-        ) {
+    function getProposalsByTaskId(uint projectId, uint taskId) public view returns (Proposal[] memory) {
+        Proposal[] memory proposals = new Proposal[](projects[projectId].tasks[taskId].proposalCount);
+        for (uint i = 0; i < projects[projectId].tasks[taskId].proposalCount; i++) {
             proposals[i] = projects[projectId].tasks[taskId].proposals[i];
         }
         return proposals;
     }
 
-    function selectWorker(
-        uint256 projectId,
-        uint256 taskId,
-        address worker
-    ) public {
-        require(
-            !projects[projectId].tasks[taskId].completed,
-            "Task is already completed"
-        );
+    function selectWorker(uint projectId, uint taskId, address worker) public {
+        require(!projects[projectId].tasks[taskId].completed, "Task is already completed");
 
         projects[projectId].tasks[taskId].selectedWorker = worker;
-        emit WorkerSelected(projectId, taskId, worker);
+        emit WorkerSelected(projectId, taskId, worker);    
     }
 
-    function completeTaskWorker(uint256 projectId, uint256 taskId) public {
-        require(
-            msg.sender == projects[projectId].tasks[taskId].selectedWorker,
-            "Only selected worker can complete the task"
-        );
+    function completeTaskWorker(uint projectId, uint taskId) public {
+        require(msg.sender == projects[projectId].tasks[taskId].selectedWorker, "Only selected worker can complete the task");
 
         projects[projectId].tasks[taskId].completed = true;
 
         emit TaskCompleted(projectId, taskId);
     }
 
-    // function reviewTask(){}
+    function reviewTask(uint projectId, uint taskId) public {
+        require(msg.sender == projects[projectId].projectManager, "Only project manager can review Task");
+        require(projects[projectId].tasks[taskId].completed, "Task is not completed yet");
 
-    function releasePayment(uint256 projectId, uint256 taskId) public payable {
-        require(
-            msg.sender == projects[projectId].projectManager,
-            "Only project manager can release payment"
-        );
-        require(
-            projects[projectId].tasks[taskId].completed,
-            "Task is not completed yet"
-        );
+        projects[projectId].tasks[taskId].reviewed = true;
 
-        uint256 stakedAmount = projects[projectId].tasks[taskId].stakedAmount;
+        emit ReviewCompleted(projectId, taskId);
+    }
+
+
+    function releasePayment(uint projectId, uint taskId) payable public {
+        require(msg.sender == projects[projectId].projectManager, "Only project manager can release payment");
+        require(projects[projectId].tasks[taskId].reviewed, "Task is not reviewed yet");
+
+        uint stakedAmount = projects[projectId].tasks[taskId].stakedAmount;
         address worker = projects[projectId].tasks[taskId].selectedWorker;
 
         payable(worker).transfer(stakedAmount);
 
         emit PaymentReleased(projectId, taskId, worker, stakedAmount);
+    
     }
 
-    // function to get a list of all projects
-    function getAllProjects() public view {}
 }
