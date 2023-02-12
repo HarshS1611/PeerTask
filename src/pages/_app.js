475cb5c6adeeeda1ecb54b105dfaa513e67632e6
Sahas <sahasvivek001@gmail.com>
2023-02-12 19:38:04 +0530

    feat:arcana auth

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,5 +1,13 @@
 import '@/styles/globals.css'
+import { AuthProvider } from "@arcana/auth";
+import { ProvideAuth } from "@arcana/auth-react";
+
+const provider = new AuthProvider(`34f13018df2e380560b5784d0eb0079401f0d02c`) // required
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <ProvideAuth provider={provider}>
+      <Component {...pageProps} />
+    </ProvideAuth>
+  )
 }
