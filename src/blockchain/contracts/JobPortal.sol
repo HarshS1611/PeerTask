d9756e1f3ad4989ffe5c43ba31f110fbb9ac065e
pranavvdesai <pranavdesai.psd@gmail.com>
2023-02-12 19:03:39 +0530

    feat: URI

--- src/blockchain/contracts/JobPortal.sol
+++ src/blockchain/contracts/JobPortal.sol
@@ -3,6 +3,7 @@ pragma solidity ^0.8.17;
 
 contract JobPortal{
     struct Project {
+        string projectURI;
         uint projectId;
         address projectManager;
         mapping(uint => Task) tasks; //Task[] tasks;
@@ -10,6 +11,7 @@ contract JobPortal{
     }
 
      struct Task {
+        string taskURI;
         uint taskId;
         uint stakedAmount;
         mapping(uint => Proposal) proposals; //Proposal[] proposals;
@@ -20,6 +22,7 @@ contract JobPortal{
     }
 
     struct Proposal {
+        string proposalURI;
         address freelancer;
         uint bidAmount;
         string motivation;
@@ -28,22 +31,23 @@ contract JobPortal{
     mapping(uint => Project) public projects; //Project[] public projects;
     uint public projectCount;
     
-    event NewProject(uint projectId, address projectManager);
-    event NewTask(uint projectId, uint taskId, uint stakedAmount);
-    event NewProposal(uint projectId, uint taskId, address freelancer, uint bidAmount);
+    event NewProject(uint projectId, address projectManager, string projectURI);
+    event NewTask(uint projectId, uint taskId, uint stakedAmount, string taskURI);
+    event NewProposal(uint projectId, uint taskId, address freelancer, uint bidAmount, string proposalURI);
     event WorkerSelected(uint projectId, uint taskId, address worker);
     event TaskCompleted(uint projectId, uint taskId);
     event ReviewCompleted(uint projectId, uint taskId);
     event PaymentReleased(uint projectId, uint taskId, address worker, uint amount);
 
-     function createProject(address projectManager) public {
+     function createProject(string calldata projectURI) public {
+        projects[projectCount].projectURI = projectURI;
         projects[projectCount].projectId = projectCount;
-        projects[projectCount].projectManager = projectManager;
+        projects[projectCount].projectManager = msg.sender;
         projects[projectCount].taskCount = 0;
 
         projectCount++;
 
-        emit NewProject(projectCount-1, projectManager);
+        emit NewProject(projectCount-1, msg.sender, projectURI);
     }
 
     function getCurrentProjectId() public view returns (uint) {
@@ -56,6 +60,7 @@ contract JobPortal{
     }
 
     function getTaskData(uint projectId, uint taskId) public view returns (
+        string memory taskURI,
         uint Id,
         uint stakedAmount,
         uint proposalCount,
@@ -64,34 +69,36 @@ contract JobPortal{
         bool isReviewed
     ) {
         Task storage task = projects[projectId].tasks[taskId];
-        return (task.taskId, task.stakedAmount, task.proposalCount, task.selectedWorker, task.completed, task.reviewed);
+        return (task.taskURI, task.taskId, task.stakedAmount, task.proposalCount, task.selectedWorker, task.completed, task.reviewed);
     }
 
 
-    function createTask(uint projectId, uint stakedAmount) public {
+    function createTask(uint projectId, uint stakedAmount, string calldata taskURI) public {
         require(msg.sender == projects[projectId].projectManager, "Only project manager can create tasks");
 
+        projects[projectId].tasks[projects[projectId].taskCount].taskURI = taskURI;
         projects[projectId].tasks[projects[projectId].taskCount].taskId = projects[projectId].taskCount;
         projects[projectId].tasks[projects[projectId].taskCount].stakedAmount = stakedAmount;
         projects[projectId].tasks[projects[projectId].taskCount].completed = false;
         projects[projectId].tasks[projects[projectId].taskCount].reviewed = false;
         projects[projectId].taskCount++;
 
-        emit NewTask(projectId, projects[projectId].taskCount-1, stakedAmount);
+        emit NewTask(projectId, projects[projectId].taskCount-1, stakedAmount, taskURI);
     }
 
-    function sendProposal(uint projectId, uint taskId, uint bidAmount, string memory motivation) public {
+    function sendProposal(uint projectId, uint taskId, uint bidAmount, string memory motivation, string calldata proposalURI) public {
         uint proposalCount = projects[projectId].tasks[taskId].proposalCount;
         address selectedWorker = projects[projectId].tasks[taskId].selectedWorker;
 
         require(selectedWorker == address(0), "Freelancer has already been selected for this task");
         
+        projects[projectId].tasks[taskId].proposals[proposalCount].proposalURI = proposalURI;
         projects[projectId].tasks[taskId].proposals[proposalCount].freelancer = msg.sender;
         projects[projectId].tasks[taskId].proposals[proposalCount].bidAmount = bidAmount;
         projects[projectId].tasks[taskId].proposals[proposalCount].motivation = motivation;
         projects[projectId].tasks[taskId].proposalCount++;
 
-        emit NewProposal(projectId, taskId, msg.sender, bidAmount);
+        emit NewProposal(projectId, taskId, msg.sender, bidAmount, proposalURI);
     }
 
     function getProposalsByTaskId(uint projectId, uint taskId) public view returns (Proposal[] memory) {
