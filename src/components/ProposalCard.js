05638108aa57c6849d5c9e31b67ae42ccccaa269
pranavvdesai <pranavdesai.psd@gmail.com>
2023-02-19 22:32:59 +0530

    fic: img

--- src/components/ProposalCard.js
+++ src/components/ProposalCard.js
@@ -32,21 +32,7 @@ export default function ProposalCard({ proposal }) {
     return (
         <div className="my-10 mx-8 roomcard border-2 border-slate-700 rounded-xl">
             <div className="rounded overflow-hidden h-full">
-                <div style={{
-                    display: "flex", alignContent: "center", justifyContent: "center",
-                    borderRadius: "100px",
-                    overflow: 'hidden',
-                    width: '200px', height: '200px'
-                    , margin: 'auto'
-                }}>
-                    <Image src={
-                        "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSqu39eyj7mkHZ2gnUmKmU9smZN8F3mI7xeC2DFXhTWwOSiL7JaliiMiC8NF3hZK-m1AD8&usqp=CAU"
-                    }
-                        width={200}
-                        height={200}
-                        alt='sample'
-                    />
-                </div>
+                
                 <div className="px-6 py-4">
                     <div className="flex">
                         <h1 className="text-white mb-2 px-2 font-bold text-lg">
